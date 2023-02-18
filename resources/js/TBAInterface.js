de7c7ce5eab7b126877b4db2bf92b4f975b928c6
sschrisman <ss.chrisman@comcast.net>
2023-02-18 13:04:42 -0500

    Update TBAInterface.js
    Add BlueAlliance API token.

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
-// TBAInterface funcitons to pull data from TheBlueAlliance.com
+// TBAInterface functions to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "0vJ6qWJkM81BcgimgbCX6aNrtsoR1S8xsXzpzd4vAX9uv0pH9sKzhux47fv4347D";
 /**
  * Get list of teams in event
  *

328ddcb1fcda798557befefbfa7d6114222f56ee
Chrisman <ss.chrisman@comcast.net>
2025-02-10 17:35:07 -0500

    Set event to IN District Mishawaka

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025ilpe",
+      "defaultValue": "2025inmis",
       "required": "true"
     },
     { "name": "Match Level",
